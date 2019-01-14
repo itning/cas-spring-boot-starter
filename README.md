e6e6b8a234b48ceeef2a19fb7ee0bc14c09dabda
itning <itning@itning.top>
2019-01-14 13:25:09 +0800

    init

--- README.md
+++ README.md
@@ -11,3 +11,28 @@
 [![GitHub release](https://img.shields.io/github/release/itning/cas-spring-boot-starter.svg)]()
 [![GitHub repo size in bytes](https://img.shields.io/github/repo-size/itning/cas-spring-boot-starter.svg)]()
 [![language](https://img.shields.io/badge/language-JAVA-orange.svg)]()
+[![](https://jitpack.io/v/itning/cas-spring-boot-starter.svg)](https://jitpack.io/#itning/cas-spring-boot-starter)
+## 使用
+
+[![](https://jitpack.io/v/itning/cas-spring-boot-starter.svg)](https://jitpack.io/#itning/cas-spring-boot-starter)
+
+1.添加JitPack仓库到pom.xml
+
+```xml
+<repositories>
+    <repository>
+        <id>jitpack.io</id>
+        <url>https://jitpack.io</url>
+    </repository>
+</repositories>
+```
+
+2.添加依赖
+
+```xml
+<dependency>
+    <groupId>com.github.itning</groupId>
+    <artifactId>cas-spring-boot-starter</artifactId>
+    <version>1.0.0-RELEASE</version>
+</dependency>
+```
--- src/main/java/top/itning/cas/RestModel.java
+++ src/main/java/top/itning/cas/RestModel.java
@@ -9,45 +9,45 @@ import java.io.Serializable;
  *
  * @author itning
  */
-public class RestModel<T> implements Serializable {
+class RestModel<T> implements Serializable {
     private int code;
     private String msg;
     private T data;
 
-    public RestModel() {
+    RestModel() {
     }
 
-    public RestModel(int code, String msg, T data) {
+    private RestModel(int code, String msg, T data) {
         this.code = code;
         this.msg = msg;
         this.data = data;
     }
 
-    public RestModel(T data) {
+    RestModel(T data) {
         this(HttpStatus.OK.value(), "查询成功", data);
     }
 
-    public int getCode() {
+    int getCode() {
         return code;
     }
 
-    public void setCode(int code) {
+    void setCode(int code) {
         this.code = code;
     }
 
-    public String getMsg() {
+    String getMsg() {
         return msg;
     }
 
-    public void setMsg(String msg) {
+    void setMsg(String msg) {
         this.msg = msg;
     }
 
-    public T getData() {
+    T getData() {
         return data;
     }
 
-    public void setData(T data) {
+    void setData(T data) {
         this.data = data;
     }
 }
